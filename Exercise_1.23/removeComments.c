cc934747e7e9a61600128802eef3f88903a3026b
Juan Carlos Mujica <mujicajuancarlos>
2016-11-06 12:33:33 -0300

    Agrego el directorio para el ejercicion 1-23 de K&R

---
+++ Exercise_1.23/removeComments.c
@@ -0,0 +1,22 @@
+/*
+
+	Ejercicio 1-23 K&R
+	Grupo #
+	Curso
+	Integrantes:
+		Juan Carlos Mujica
+
+*/
+
+#include <stdio.h>
+
+#define	EN_PROGRAMA			0
+#define	INICIO_COMENTARIO	1
+#define	EN_COMENTARIO		2
+#define	FIN_COMENTARIO		3
+#define	EN_STRING			4
+
+int main(void)
+{
+	printf("%s \n","Hola");
+}
--- HelloWorld/hello.c
+++ HelloWorld/hello.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 	
 int main(){
-	printf("Hola mundo");
+	printf("%s\n","Hola mundo");
 	return 0;
 }
