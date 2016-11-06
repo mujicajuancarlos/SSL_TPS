1c02ad6cd1a6868706edf04e1de98ee4e268dc3a
Juan Carlos Mujica <mujicajuancarlos>
2016-11-06 12:49:21 -0300

    agrego estructura inicial

--- Exercise_1.23/removeComments.c
+++ Exercise_1.23/removeComments.c
@@ -15,8 +15,15 @@
 #define	EN_COMENTARIO		2
 #define	FIN_COMENTARIO		3
 #define	EN_STRING			4
+#define EN_CARACTER			5
 
 int main(void)
 {
-	printf("%s \n","Hola");
+	int currentState, currentChar;
+	
+	currentState = EN_PROGRAMA;
+	
+	while ((currentChar = getchar()) != EOF){
+		
+	}
 }
